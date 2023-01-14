653f85077a6a30308fbbf9f0a0a3f860bae51a0a
Aly  Khaled <47189280+Aly1294@users.noreply.github.com>
2023-01-14 13:58:36 -0800

    Web Tooling Lab

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const htmlPlugin=require("html-webpack-plugin")
+const cssPlugin=require("mini-css-extract-plugin")
+const cssMinPlugin=require("css-minimizer-webpack-plugin")
+const htmlMinPlugin=require("html-minimizer-webpack-plugin")
+const jsMinPlugin=require("terser-webpack-plugin")
+// const imgMinPlugin=require("imagemin")
+const ImageConfigWebpackPlugin = require('image-config-webpack-plugin');
+module.exports = {
+  mode:"production",
+  entry: './src/entry_index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  optimization:{
+    minimizer:[
+      new cssMinPlugin(),
+      new htmlMinPlugin(),
+      new jsMinPlugin(),
+      // new imgMinPlugin(),
+      new ImageConfigWebpackPlugin(),
+    ]
+  },
+
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        // use: [ "css-loader"],
+        use:[cssPlugin.loader, "css-loader" ],
+      },
+    ],
+  },
+  plugins:[
+    new htmlPlugin({
+      template:"./index.html",
+      filename:"home.html",
+      inject:"body"
+    }),
+    new cssPlugin({
+      filename:"home.css"
+    })
+  ],
+  devServer:{
+    compress:true,
+    port:9000
+  }
+  ,
+  
+};
